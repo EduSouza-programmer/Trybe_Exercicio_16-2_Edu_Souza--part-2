c25e4704188b31e77a5fccbd31a40ecf72b87068
Eduardo Souza <dado.eletrons@gmail.com>
2020-12-07 17:06:22 -0300

    Updates

---
+++ precache-manifest.96e9ba7314827fb1682953756f1f3ef0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fb1df83b30deef6a8ef3436addb888",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/index.html"
+  },
+  {
+    "revision": "d67047b99886f9e7d4c6",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/css/main.b570d59e.chunk.css"
+  },
+  {
+    "revision": "d1b3783dceeeab2030a3",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/js/2.bd434558.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/js/2.bd434558.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d67047b99886f9e7d4c6",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/js/main.ecc7bbde.chunk.js"
+  },
+  {
+    "revision": "3fc248b0294736bda317",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/js/runtime-main.008f477a.js"
+  },
+  {
+    "revision": "38352d9ee02244a39dffad1621609212",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/media/carBlue.38352d9e.jpeg"
+  },
+  {
+    "revision": "a6362bdc70f08eb38f428bf8c3895cd3",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/media/carRed.a6362bdc.jpeg"
+  },
+  {
+    "revision": "a1afa5a3cdd10b52a2df7bde7bdb7d1e",
+    "url": "/Trybe_Exercicio_16-2_Edu_Souza--part-2/static/media/carYellow.a1afa5a3.jpeg"
+  }
+]);
